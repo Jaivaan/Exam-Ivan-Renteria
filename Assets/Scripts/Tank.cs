aceee8e3c37c64f72c608372ff6551fe91054bd3
Jaivaan <ivan.renteria@opendeusto.es>
2024-06-04 10:01:33 +0200

    die

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Tank : MonoBehaviour
 {
@@ -47,5 +48,14 @@ public class Tank : MonoBehaviour
         moveHorizontal *= Time.deltaTime;
 
         transform.Translate(moveHorizontal, 0, moveVertical);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("Start");
+        }
     }
 }
